36e69ef32fa49f9e4acfbab541d726c4cc499979
bmanga <bruno.manga95@gmail.com>
2019-09-07 21:00:17 +0200

    Update install.sh
    added conan-community remote

--- .ci/install.sh
+++ .ci/install.sh
@@ -23,3 +23,4 @@ pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
 
 conan user
+conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
