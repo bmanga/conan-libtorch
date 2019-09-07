d3b63510db8c75ff2e108565b0ed20315903074b
bmanga <bruno.manga95@gmail.com>
2019-09-07 21:49:58 +0200

    add config_url

--- build.py
+++ build.py
@@ -5,6 +5,7 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
+    config_url = "https://github.com/bincrafters/conan-config.git"
 
     builder = build_template_default.get_builder()
 
